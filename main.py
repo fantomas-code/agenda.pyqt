fe4fdfc1a22127d544b78e6e38fad0e7d52759dc
eyllanesc <e.yllanescucho@gmail.com>
2016-12-06 05:02:41 -0500

    primer commit :D

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+#!/usr/bin/env python
+
 import sys
 
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtWidgets import QSystemTrayIcon
+
 from DiaryMainWindow import DiaryMainWindow
 
 
@@ -11,6 +16,14 @@ def app():
     app.setOrganizationName('Diary')
     app.setOrganizationDomain('Diary.com')
     app.setApplicationVersion('0.1')
+
+    if not QSystemTrayIcon.isSystemTrayAvailable():
+        QMessageBox.critical(None, "Systray",
+                                   "I couldn't detect any system tray on this system.")
+        sys.exit(1)
+
+    QApplication.setQuitOnLastWindowClosed(False)
+
     w = DiaryMainWindow()
     w.show()
     sys.exit(app.exec_())
